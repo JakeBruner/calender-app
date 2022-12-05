3a885bdaa39049e548e1f6cbd4ad0172addba131
JakeBruner <74114605+JakeBruner@users.noreply.github.com>
2022-12-05 11:36:22 -0500

    adding trpc prodecure

--- src/pages/api/restricted.ts
+++ src/pages/api/restricted.ts
@@ -1,21 +1,44 @@
-import { type NextApiRequest, type NextApiResponse } from "next";
+// import { type NextApiRequest, type NextApiResponse } from "next";
 
-import { getServerAuthSession } from "../../server/common/get-server-auth-session";
+// import { prisma } from "../../server/db/client";
 
-const restricted = async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getServerAuthSession({ req, res });
+// import { getServerAuthSession } from "../../server/common/get-server-auth-session";
 
-  if (session) {
-    res.send({
-      content:
-        "This is protected content. You can access this content because you are signed in.",
-    });
-  } else {
-    res.send({
-      error:
-        "You must be signed in to view the protected content on this page.",
-    });
-  }
-};
+// export const bookings = async (req: NextApiRequest, res: NextApiResponse) => {
+//   const session = await getServerAuthSession({ req, res });
 
-export default restricted;
+//   if (session) {
+//     try {
+//       const currentMonth = req.query.month;
+//       if (typeof currentMonth !== "number") {
+//         throw new Error("Invalid month");
+//       }
+//       const bookings = await getBookings(currentMonth);
+//       res.status(200).json({ bookings });
+//     } catch (err: unknown) {
+//       res.status(500).json({ message: err?.message });
+//     }
+//   } else {
+//     res.send({
+//       error: "You must be signed in to access the bookings API.",
+//     });
+//   }
+// };
+
+// async function getBookings(
+//   currentMonth: number,
+//   currentYear: number
+// ): Promise<prisma.Booking[]> {
+//   const startOfMonth = new Date(currentYear, currentMonth, 1);
+//   const endOfMonth = new Date(currentYear, currentMonth + 1, 0);
+
+//   return await prisma.booking.findMany({
+//     where: {
+//       OR: [
+//         { start: { gte: startOfMonth, lte: endOfMonth } },
+//         { end: { gte: startOfMonth, lte: endOfMonth } },
+//       ],
+//     },
+//   });
+// }
+// export default bookings;
